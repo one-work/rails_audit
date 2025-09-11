17dc46f6770396055201f434f9f1da740a76e36f
qinmingyuan <mingyuan0715@foxmail.com>
2025-09-11 20:00:18 +0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,37 +1,35 @@
 Rails.application.routes.draw do
-  scope RailsCom.default_routes_scope do
-    namespace :auditor, defaults: { business: 'auditor' } do
-      scope ':audited_type/:audited_id', module: 'admin', defaults: { namespace: 'admin' } do
-        resources :audits, only: [:index] do
+  namespace :auditor, defaults: { business: 'auditor' } do
+    scope ':audited_type/:audited_id', module: 'admin', defaults: { namespace: 'admin' } do
+      resources :audits, only: [:index] do
+        collection do
+          post '/' => :index
+        end
+      end
+    end
+
+    scope ':approving_type/:approving_id', module: 'admin', defaults: { namespace: 'admin' } do
+      resources :approvals, only: [:index]
+    end
+
+    namespace :admin, defaults: { namespace: 'admin' } do
+      resources :approvals, except: [:index, :new, :create]
+      scope path: ':verifiable_type/:verifiable_id' do
+        resources :verifiers do
           collection do
-            post '/' => :index
+            get :members
           end
         end
       end
-
-      scope ':approving_type/:approving_id', module: 'admin', defaults: { namespace: 'admin' } do
-        resources :approvals, only: [:index]
+      scope path: ':verified_type/:verified_id' do
+        resources :verifications
       end
+    end
 
-      namespace :admin, defaults: { namespace: 'admin' } do
-        resources :approvals, except: [:index, :new, :create]
-        scope path: ':verifiable_type/:verifiable_id' do
-          resources :verifiers do
-            collection do
-              get :members
-            end
-          end
-        end
-        scope path: ':verified_type/:verified_id' do
-          resources :verifications
-        end
-      end
-
-      namespace :panel, defaults: { namespace: 'panel' } do
-        resources :audits do
-          member do
-            post :restore
-          end
+    namespace :panel, defaults: { namespace: 'panel' } do
+      resources :audits do
+        member do
+          post :restore
         end
       end
     end
